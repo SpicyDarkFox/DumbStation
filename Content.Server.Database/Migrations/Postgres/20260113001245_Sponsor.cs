4cf20f82c2d376be8858344630fe97dd7a21e42b
SpicyDarkFox <foxyflutter6@gmail.com>
2026-01-13 03:13:42 +0300

    добавление в бд

---
+++ Content.Server.Database/Migrations/Postgres/20260113001245_Sponsor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Postgres
+{
+    /// <inheritdoc />
+    public partial class Sponsor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "sponsors",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tier = table.Column<int>(type: "integer", nullable: false),
+                    ooccolor = table.Column<string>(type: "text", nullable: false),
+                    have_priority_join = table.Column<bool>(type: "boolean", nullable: false),
+                    allowed_markings = table.Column<string>(type: "text", nullable: false),
+                    extra_slots = table.Column<int>(type: "integer", nullable: false),
+                    allow_job = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sponsors", x => x.user_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sponsors");
+        }
+    }
+}
---
+++ Content.Server.Database/Migrations/Sqlite/20260113001218_Sponsor.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Content.Server.Database.Migrations.Sqlite
+{
+    /// <inheritdoc />
+    public partial class Sponsor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "sponsors",
+                columns: table => new
+                {
+                    user_id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    tier = table.Column<int>(type: "INTEGER", nullable: false),
+                    ooccolor = table.Column<string>(type: "TEXT", nullable: false),
+                    have_priority_join = table.Column<bool>(type: "INTEGER", nullable: false),
+                    allowed_markings = table.Column<string>(type: "TEXT", nullable: false),
+                    extra_slots = table.Column<int>(type: "INTEGER", nullable: false),
+                    allow_job = table.Column<bool>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_sponsors", x => x.user_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "sponsors");
+        }
+    }
+}
